d9a87f61c5309176fc35436a998227863cdbbf03
Juan Giménez <juanp.gi2485@gmail.com>
2022-07-22 15:58:27 +0100

    button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Signup from "./components/Signup";
 import HomePage from "./components/HomePage";
 import AuthContextProvider from "./contexts/AuthContext";
 import LandingPage from "./components/LandingPage";
 import MyNotes from "./components/MyNotes";
 import "./App.css"
+import { AuthContext } from "./contexts/AuthContext";
 
 // ! FALTA CRIAR OS PROTECTED ROUTES!!
+//create protected routes
+// const ProtectedRoute = ({ element, ...rest }) => {
+//   const { auth } = React.useContext(AuthContext);
+
+//   const navigate = useNavigate();
+
+//   return (
+//     <Route
+//       {...rest}
+//       render={({ location }) =>
+//         auth ? (
+//           element
+//         ) : (
+//           navigate("/login", { replace: true })
+//         )
+//       }
+//     />
+//   );
+// }
+
 
 function App() {
   return (
@@ -17,8 +38,8 @@ function App() {
           <Routes>
             <Route path="/login" element={<LandingPage/>} />
             <Route path="/signup" element={<Signup/>} />
-            <Route path="/homepage" element={<HomePage/>} />
-            <Route path="/my-notes" element={<MyNotes />} />
+              <Route path="/homepage" element={<HomePage/>} />
+              <Route path="/my-notes" element={<MyNotes/>} />
             <Route path="*" element={<Navigate to="/login" replace/>} />
           </Routes>
         </BrowserRouter>
@@ -26,6 +47,8 @@ function App() {
     </div>
   );
 }
+           
+          
      
         
 
