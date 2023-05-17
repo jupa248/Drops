822efb13cf988c331c3d5ea508f8ecc10ddca0b4
Juan Giménez <juanp.gi2485@gmail.com>
2023-05-17 09:33:03 +0100

    delete files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import Profile from "./components/Profile";
-import AuthContextProvider from "./contexts/AuthContext";
-
-// ! FALTA CRIAR OS PROTECTED ROUTES!!
-
 function App() {
-  return (
-    <div className="App">
-      <AuthContextProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login/>} />
-            <Route path="/signup" element={<Signup/>} />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="*" element={<Navigate to="/login" replace/>} />
-          </Routes>
-        </BrowserRouter>
-      </AuthContextProvider>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
